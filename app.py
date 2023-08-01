a8a4d13cc412478a6c0e12408820c4737879c934
Nitish-py <nsqwertfb@gmail.com>
2023-08-01 16:20:22 +0530

    final

--- app.py
+++ app.py
@@ -13,12 +13,11 @@ from langchain.memory import ConversationBufferMemory
 import streamlit as st
 import os
 
-os.environ['OPEN_API_KEY'] ='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'
-
-def db(texts,text_splitter,name):
+def db(texts,text_splitter,api):
+    
     chunks = text_splitter.split_text(texts)
     embeddings = OpenAIEmbeddings()
-    db = Chroma.from_texts(chunks, embeddings,index_name="pdfgpt",namespace=name)
+    db = Chroma.from_texts(chunks, embeddings)
     retriever = db.as_retriever(search_type="similarity", search_kwargs={"k":2})
     memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
     qa = ConversationalRetrievalChain.from_llm(llm=OpenAI(temperature=0), retriever=retriever, chain_type="refine",memory=memory)
@@ -32,11 +31,20 @@ def ai(prompt):
     #prompt=system_prompt+str(": question is :")+prompt
     result = qa({"question": prompt,  "chat_history": chat_history})
     return result["answer"]
-
+def set_environment_variable(api_key):
+    # Set the environment variable for the OpenAI API key
+    os.environ["OPENAI_API_KEY"] = api_key
 def main():
     global qa, chat_history
     placeholder=st.empty()
     placeholder.empty()
+    placeholder.title("your openai api key")
+    global api
+    api=st.text_input("enter here")
+    if st.button("Load API Key"):
+        # Set the environment variable with the provided API key
+        set_environment_variable(api)
+        st.success("API key loaded successfully!")
     placeholder.title("Upload or Chat PDF")
     #st.header("PDF/URL QA")
     #global system_prompt
@@ -47,8 +55,6 @@ def main():
         pdf = st.file_uploader("Upload your PDF", type="pdf")
         if pdf is not None:
             #print(pdf)
-            name=st.text_input("Name of your pdf")
-            name=name.lower()
             pdf_reader = PdfReader(pdf)
             texts = ""
             for page in pdf_reader.pages:
@@ -58,7 +64,7 @@ def main():
             chunk_size = 1000,
             chunk_overlap = 0
         )
-            qa=db(texts,text_splitter,name)
+            qa=db(texts,text_splitter,api)
             chat_history = []
             st.header("PDF/URL QA")
             query = st.text_input("Ask a question in PDF")
@@ -70,14 +76,15 @@ def main():
     elif page == 'Random talk':
             chat_history=[]
             st.header("Start Chatting")
-            name=st.text_input("Your message")
-            prompt = "\"Act like a personal assistant. You can respond to questions, translate sentences, summarize news, and give recommendations. " + user_message + "\""
-            # Call the OpenAI Api to process our prompt
-            openai_response = openai.Completion.create(model="text-davinci-003", prompt=prompt,max_tokens=4000)
-            print("openai response:", openai_response)
-            # Parse the response to get the response text for our prompt
-            response_text = openai_response.choices[0].text
-            st.write( response_text)
+            message=st.text_input("Your message")
+            if st.button('reply'):
+                prompt = "\"Act like a personal assistant. You can respond to questions, translate sentences, summarize news, and give recommendations. " + message + "\""
+                # Call the OpenAI Api to process our prompt
+                openai_response = openai.Completion.create(model="text-davinci-003", prompt=prompt,max_tokens=4000)
+                print("openai response:", openai_response)
+                # Parse the response to get the response text for our prompt
+                response_text = openai_response.choices[0].text
+                st.write( response_text)
 
 
 if __name__ == "__main__":
